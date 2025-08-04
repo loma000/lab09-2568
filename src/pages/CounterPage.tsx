d3c687c53c9ff97f9d81a6c510499784f48a7702
loma000 <90094687+loma000@users.noreply.github.com>
2025-08-04 21:38:14 +0700

    a2

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 export default function CounterPage() {
+
+const [counter,setCouter] = useState(0);
+
+const counterplus=()=>{
+  setCouter(counter+1);
+};
+
+
   return (
     <div className="container text-center">
       <h2>Counter Page</h2>
       {/* Result Text */}
-      <p>Counter:</p>
-      <button>Increase</button>
+      <p>Counter: {counter}</p>
+      <button onClick={counterplus}>Increase</button>
     </div>
   );
 }
